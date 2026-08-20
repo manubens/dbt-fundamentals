aa6d99c9075501875f31098d950fc8d2ab001599
Rodrigo Manubens <rodrigo.manubens@gmail.com>
2026-08-20 14:32:53 -0600

    don't know what most of these are

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,7 @@ SELECT
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
     created as created_at
 FROM {{ source('stripe', 'payments') }}
